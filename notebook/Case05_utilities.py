c7e4547f7461384b89cb1c4015900e0080e718b7
caiwei <caiwei@stanford.edu>
2018-08-29 13:11:50 -0700

    Prepare Case05 for running in Python shell

---
+++ notebook/Case05_utilities.py
@@ -0,0 +1,192 @@
+import numpy as np
+import scipy as sp
+import scipy.sparse as spm
+from scipy.io import loadmat, savemat
+
+import matplotlib.pyplot as plt
+from matplotlib import cm, colors
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+import sys, time, os.path
+from itertools import permutations
+sys.path.append('../module/')
+from SHUtil import SphCoord_to_CartCoord, CartCoord_to_SphCoord
+
+import pyshtools
+from SHUtil import SHCilmToVector, SHVectorToCilm, lmk2K, K2lmk
+from SHUtil import plotfv, TransMat, l_coeffs, m_coeffs
+from ShElastic import calSmode, calUmode
+from SHBV import generate_submat, visualize_Cmat, print_SH_mode
+
+from scipy.sparse.linalg import lsqr, spsolve
+
+# procedures for transformation between Uvec and Tvec
+def Uvec2Tvec(Uvec, Cmat, Dmat, disp=False):
+    tic = time.time()
+    #B = lsqr(Dmat, Uvec.T)
+    B_sol = spsolve(Dmat, Uvec.T)
+    toc = time.time()
+    #B_sol = B[0]
+    #print('Residual:', B[3], 'Time:', toc-tic, 'Solution:', B_sol.size)
+    print('Time: %.4fs'%(toc-tic))
+    if disp:
+        disp_index_sol = print_SH_mode(B_sol, m_dir=3, etol=1e-8)
+    return Cmat.dot(B_sol)
+
+def Tvec2Uvec(Tvec, Cmat, Dmat, disp=False):
+    tic = time.time()
+    A = lsqr(Cmat, Tvec.T, atol=0, btol=0, conlim=0)
+    #A_sol = spsolve(Cmat, Tvec.T)
+    toc = time.time()
+    A_sol = A[0]
+    print('Residual:', A[3], 'Time:', toc-tic, 'Solution:', A_sol.size)
+    print('Time: %.4fs'%(toc-tic))
+    if disp:
+        disp_index_sol = print_SH_mode(A_sol, m_dir=3, etol=1e-8)
+    return Dmat.dot(A_sol)
+
+def SHVec2mesh(xvec, lmax=None, SphCoord=True, Complex=False):
+    if lmax is None:
+        lmax = (np.sqrt(xvec.size/3) - 1).astype(np.int)
+    cvec = xvec.reshape(3, -1)
+    nvec = cvec.shape[1]
+    xmesh= [None for _ in range(3)]
+    for k in range(3):
+        if Complex:
+            cext = np.zeros((lmax+1)**2, dtype=np.complex)
+            cext[:nvec] = cvec[k, :(lmax+1)**2]
+            cilm = SHVectorToCilm(cext)
+        else:
+            cext = np.zeros((lmax+1)**2)
+            cext[:nvec] = cvec[k, :(lmax+1)**2]
+            cilm = pyshtools.shio.SHVectorToCilm(cext)
+        coeffs = pyshtools.SHCoeffs.from_array(cilm)
+        grid = coeffs.expand('GLQ')
+        xmesh[k] = grid.to_array().real
+    xmesh = np.stack(xmesh, axis=-1)
+    if SphCoord:
+        Q = TransMat(lJmax=lmax)
+        xmesh = np.sum(Q*xmesh[...,np.newaxis,:], axis=-1)
+    return xmesh
+
+# visualizing SHvectors in 2D
+def visSHVec(xvec, lmax_plot=None, cmap='viridis', show=True, 
+             SphCoord=True, config_quiver=(2, 4, 'k', 1), n_vrange=None, s_vrange=None,
+             lonshift=0, Complex=False, figsize=(10, 5)):
+    xmesh = SHVec2mesh(xvec, lmax=lmax_plot, SphCoord=SphCoord, Complex=Complex)
+    if SphCoord:
+        fig = [None for _ in range(2)]
+        ax = [None for _ in range(2)]
+        xshear= np.linalg.norm(xmesh[...,1:], axis=-1)
+        
+        fig[0], ax[0] = plotfv(xmesh[...,0], show=False, cmap=cmap,vrange=n_vrange,
+                               lonshift=lonshift, figsize=figsize)
+        ax[0].set_title('norm')
+        
+        fig[1], ax[1] = plotfv(xshear, show=False, cmap='Reds', lonshift=lonshift, figsize=figsize, vrange=s_vrange)
+        latsdeg, lonsdeg = pyshtools.expand.GLQGridCoord(lmax_plot)
+        lons, lats = np.meshgrid(lonsdeg, latsdeg)
+        xshift = np.roll(xmesh, np.round(lons.shape[1]*lonshift/360).astype(np.int), axis=1)
+        st, dq, color, scale = config_quiver
+        ax[1].quiver(lons[::dq,st::dq], lats[::dq,st::dq], 
+                     xshift[::dq,st::dq,1], xshift[::dq,st::dq,2], 
+                     color=color, scale=scale)
+        ax[1].set_title('shear')
+    else:
+        fig = [None for _ in range(3)]
+        ax = [None for _ in range(3)]
+        titlestr = ('x', 'y', 'z')
+        for k in range(3):
+            fig[k], ax[k] = plotfv(xmesh[...,k], show=False, cmap=cmap, lonshift=lonshift, figsize=figsize)
+            ax[k].set_title('$'+titlestr[k]+'$')
+    if show:
+        plt.show()
+    return fig, ax
+
+# visualizing SHvectors in 3D
+def visSH3d(xmesh, cmesh=None, r0=1, lmax_plot=None,
+            figsize=(16,16), show=True, filename=None,
+            elevation=0, azimuth=0, surface=False, color=None):
+    if lmax_plot is None:
+        lmax_plot = xmesh.shape[0] - 1
+    lats, lons = pyshtools.expand.GLQGridCoord(lmax_plot)
+    nlat = lats.size; nlon = lons.size;
+
+    lats_circular = np.hstack(([90.], lats, [-90.]))
+    lons_circular = np.append(lons, [lons[0]])
+    u = np.radians(lons_circular)
+    v = np.radians(90. - lats_circular)
+    normvec = np.zeros((nlat+2, nlon+1, 3))
+    normvec[...,0] = np.sin(v)[:, None] * np.cos(u)[None, :]
+    normvec[...,1] = np.sin(v)[:, None] * np.sin(u)[None, :]
+    normvec[...,2] = np.cos(v)[:, None] * np.ones_like(lons_circular)[None, :]
+
+    upoints = np.zeros((nlat + 2, nlon + 1, 3))
+    upoints[1:-1, :-1, :] = xmesh
+    upoints[0, :, :] = np.mean(xmesh[0,:,:], axis=0)  # not exact !
+    upoints[-1, :, :] = np.mean(xmesh[-1,:,:], axis=0)  # not exact !
+    upoints[1:-1, -1, :] = xmesh[:, 0, :]
+    upoints *= r0
+    
+    x = r0 * np.sin(v)[:, None] * np.cos(u)[None, :]  + upoints[..., 0]
+    y = r0 * np.sin(v)[:, None] * np.sin(u)[None, :] + upoints[..., 1]
+    z = r0 * np.cos(v)[:, None] * np.ones_like(lons_circular)[None, :] + upoints[...,2]
+
+    if color is None:
+        if cmesh is None:
+            magn_point = np.sum(normvec * upoints, axis=-1)
+        else:
+            tpoints = np.zeros((nlat + 2, nlon + 1, 3))
+            tpoints[1:-1, :-1, :] = cmesh
+            tpoints[0, :, :] = np.mean(cmesh[0,:,:], axis=0)  # not exact !
+            tpoints[-1, :, :] = np.mean(cmesh[-1,:,:], axis=0)  # not exact !
+            tpoints[1:-1, -1, :] = cmesh[:, 0, :]
+            magn_point = np.sum(normvec * tpoints, axis=-1)
+        magn_face = 1./4. * (magn_point[1:, 1:] + magn_point[:-1, 1:] +
+                             magn_point[1:, :-1] + magn_point[:-1, :-1])
+        magnmax_face = np.max(np.abs(magn_face))
+        magnmax_point = np.max(np.abs(magn_point))
+        norm = plt.Normalize(-magnmax_face / 2., magnmax_face / 2., clip=True)
+        cmap = plt.get_cmap('RdBu_r')
+        colors = cmap(norm(magn_face.flatten()))
+        colors = colors.reshape(nlat + 1, nlon, 4)
+
+    fig = plt.figure(figsize=figsize)
+    ax3d = fig.add_subplot(1, 1, 1, projection='3d')
+    if surface:
+        if color is None:
+            ax3d.plot_surface(x, y, z, rstride=1, cstride=1, facecolors=colors)
+        else:
+            ax3d.plot_surface(x, y, z, rstride=1, cstride=1, color=color)
+    else:
+        ax3d.scatter(x, y, z)
+    ax3d.view_init(elev=elevation, azim=azimuth)
+    ax3d.tick_params(labelsize=16)
+
+    if filename is not None:
+        plt.tight_layout()
+        fig.savefig(filename,transparent=True)
+    if show:
+        plt.show(block=True)
+    return fig, ax3d
+
+# convert between complex and real SH vectors
+
+def SHvec_rtoc(xvec):
+    lmax = np.int(np.sqrt(xvec.size/3)-1)
+    rcilm = pyshtools.shio.SHVectorToCilm(xvec)
+    ccilm = pyshtools.shio.SHrtoc(rcilm)
+    cilm = np.zeros_like(ccilm, dtype=np.complex)
+    cilm[0,...] = ccilm[0,...] + 1.j*ccilm[1,...]
+    cilm[1,:,1:] = (-1)**m_coeffs(lmax)[0,:,1:] * cilm[0,:,1:].conj()
+    return SHCilmToVector(cilm)
+
+def SHvec_ctor(xvec):
+    cilm = SHVectorToCilm(xvec)
+    ccilm = np.zeros_like(cilm, dtype=np.float)
+    ccilm[0,...] = cilm[0,...].real
+    ccilm[1,...] = cilm[0,...].imag
+    rcilm = pyshtools.shio.SHctor(ccilm)
+    return pyshtools.shio.SHCilmToVector(rcilm)
+
+
